4ec8c828975cd6acf2caed03ee86868c6379b16c
NIKHIL BN <nikhilbnaradhya1.gem@gmail.com>
2019-03-31 17:49:23 +0530

    Added ejs as the view engine

--- controllers/postMessage.js
+++ controllers/postMessage.js
@@ -1,15 +1,24 @@
 module.exports = function(app , request , urlencodedParser , url) {
-    request.post({
-        url: url,
-        json: true,
-        body: {
-            "text": "Hello! This is Slack-Node-App"
-        }
-    } , (error , res , body) => {
-        if(error) {
-            throw error;
-        }
-        //console.log(res);
-        console.log(body);
+
+    app.get('/slackmessenger' , (req , res) => {
+        res.render('slackMessage');
     });
+
+    app.post('/slackmessenger' , urlencodedParser , (req , res) => {
+        request.post({
+            url: url,
+            json: true,
+            body: {
+                "text": req.body.message
+            }
+        } , (error , res , body) => {
+            if(error) {
+                throw error;
+            }
+            //console.log(res);
+            console.log(body);
+        });
+        res.send('Thanks! Your query has succcessfully been recorded!');
+    });
+
 };
--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const postMessage = require('./controllers/postMessage');
 const request = require('request');
 const app = express();
 const urlencodedParser = express.urlencoded({ extended: false });
+const PORT = 3000 || process.env.PORT;
 
 postMessage(app , request , urlencodedParser , process.env.SLACK_URL);
 
-const PORT = 3000 || process.env.PORT;
+app.set('view engine' , 'ejs');
 
 app.listen(PORT , () => {
-    console.log('Server is running on PORT 3000...');
+    console.log(`Server is running on PORT ${PORT}...`);
 });
